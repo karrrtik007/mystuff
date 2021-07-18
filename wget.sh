6a58d2af4f3d75a73114d9662f6ba495a07b4709
karrrtik007 <74298752+karrrtik007@users.noreply.github.com>
2021-07-18 13:02:35 +0000

    Delete wget.sh

--- wget.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -i js-links.txt 
-
-echo "js files fetching done (holy server) " |  ../tools/notify -silent -discord-webhook-url https://discordapp.com/api/webhooks/845307945520857148/ZtHqQV8auIRN429rXZiK8yAekgB-CSnXhdafimJUzT2oscPOOUDAWQT4o3aYh3tGISkC -discord
